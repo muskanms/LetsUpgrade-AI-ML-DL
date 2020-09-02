b52bc1c4188e83079d682f5a829c579bd0fd3ade
muskanms <63771279+muskanms@users.noreply.github.com>
2020-09-02 22:46:15 +0530

    q5

--- assign_day_3/question_5.py
+++ assign_day_3/question_5.py
@@ -1,1 +1,11 @@
+#Write a program to convert Fahrenheit to kelvin and celsius both.
 
+
+
+F=int(input("enter the value in fahrenheit "))
+
+celsius =5/9*(F-32) 
+kelvin= 273.5+ celsius
+
+print("Fahrenheit to celsius = ",celsius)
+print("Fahrenheit to kelvin = ",kelvin)
