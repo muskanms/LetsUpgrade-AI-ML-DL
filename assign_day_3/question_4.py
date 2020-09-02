af8b071e03a3f2bace7b3b985122520a5d2a929f
muskanms <63771279+muskanms@users.noreply.github.com>
2020-09-02 22:27:16 +0530

    q4

--- assign_day_3/question_4.py
+++ assign_day_3/question_4.py
@@ -1,1 +1,10 @@
+#Write a program to swap two numbers in Python without using a temporary variable.
 
+
+
+
+a=input("first number a= ")
+b=input("second number b= ")
+a,b=b,a
+print("first number a=",a)
+print("second number b=",b)
