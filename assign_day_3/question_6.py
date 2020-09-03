669787248696e7667befde56bcdede74fa136f36
muskanms <63771279+muskanms@users.noreply.github.com>
2020-09-03 11:19:49 +0530

    q6

--- assign_day_3/question_6.py
+++ assign_day_3/question_6.py
@@ -1,1 +1,22 @@
 #Write a program to demonstrate all the available data types in Python. Hint: Use type() function.
+
+
+
+a=2.303
+b=2
+c=complex(2,3)
+d=True
+e="muskan"
+f=[2,3,4,5]
+tup=("star","sun",6,7)
+dict={"name":"muskan","stars":"sky","value":10}
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(tup))
+print(type(dict))
+
+
